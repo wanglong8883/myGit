187e68e709771da6d268e1544fc156f06685043c
wanglong8883 <774169725@qq.com>
2018-04-14 10:45:07 +0800

    add override method

--- src/netty/test1/ServerHandle.java
+++ src/netty/test1/ServerHandle.java
@@ -1,7 +1,33 @@
 package netty.test1;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
 
 public class ServerHandle extends ChannelHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf= (ByteBuf) msg;
+        byte[] req=new byte[buf.readableBytes()];
+        buf.readBytes(req);
+
+        String body=new String(req,"utf-8");
+        System.out.println("Server: "+body);
+
+        //服务器端给客户端响应
+        String response="Hi Client!!!";
+        ctx.writeAndFlush(Unpooled.copiedBuffer(response.getBytes()));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Server channel active ...");
+    }
 
 }
